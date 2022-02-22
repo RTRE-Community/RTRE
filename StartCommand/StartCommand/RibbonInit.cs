53a8aa0e8ed3c31005dc40ebab9ee7fb5f672b6d
TranDennis <dennishoang2010@gmail.com>
2022-02-22 11:31:10 +0100

    Init RTRE revit plugin

---
+++ StartCommand/StartCommand/RibbonInit.cs
@@ -0,0 +1,35 @@
+﻿using Autodesk.Revit.UI;
+using System.Reflection;
+
+namespace StartCommand
+{
+    internal class RibbonInit
+    {
+        internal void AddRibbonPanel(UIControlledApplication application)
+        {
+            const string RIBBON_TAB = "RTRE";
+            const string RIBBON_PANEL = "Sever Tasks";
+
+            application.CreateRibbonTab(RIBBON_TAB);
+
+            // Add a new ribbon panel
+            RibbonPanel ribbonPanel = application.CreateRibbonPanel(RIBBON_TAB, RIBBON_PANEL);
+
+            // Get dll assembly path
+            string thisAssemblyPath = Assembly.GetExecutingAssembly().Location;
+
+            // create push button for CurveTotalLength
+            PushButtonData b1Data = new PushButtonData(
+                "POST",
+                "POST" + System.Environment.NewLine + "  Request  ",
+                thisAssemblyPath,
+                "StartCommand.StartCommand");
+
+            PushButton pb1 = (PushButton)ribbonPanel.AddItem(b1Data);
+            pb1.ToolTip = "Communicate with the server!";
+
+        }
+
+ 
+    }
+}
