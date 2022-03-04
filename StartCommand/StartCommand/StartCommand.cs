ced7c1a2c0cded98090e6cf0f28d52e98db9aa7e
dennishoang2010 <dennishoang2010@gmail.com>
2022-03-04 11:06:29 +0100

    Revit save functionality fix

--- StartCommand/StartCommand/StartCommand.cs
+++ StartCommand/StartCommand/StartCommand.cs
@@ -11,16 +11,32 @@ namespace StartCommand
     {
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
-            pingServer();
+            pingServer(commandData);
 
             return Result.Succeeded;
         }
 
-        private async void pingServer()
+        private async void pingServer(ExternalCommandData commandData)
         {
+            UIApplication uiapp = commandData.Application;
+
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+
+
+            Document doc = uidoc.Document;
+
+            View view = uidoc.ActiveView;
 
             try
             {
+
+                Transaction tr = new Transaction(commandData.Application.ActiveUIDocument.Document); tr.Start("Command name here");
+
+                IFCExportOptions exportOptions = new IFCExportOptions();
+                doc.Export("C:/Users/Levan/Documents/RTRECOM", "IFCfilerevite", exportOptions);
+                tr.Commit();
+              
+
                 HttpClient client = new HttpClient();
 
                 var data = new StringContent("Hello from Revit", Encoding.UTF8, "application/json");
