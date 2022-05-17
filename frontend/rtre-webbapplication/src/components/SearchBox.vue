5fc387906574348124a2c9d8a8eb348406bd5e16
Dennis Tran <61258188+TranDennis@users.noreply.github.com>
2022-05-17 14:12:17 +0200

    Fixed searchbox now shows subprojects

--- frontend/rtre-webbapplication/src/components/SearchBox.vue
+++ frontend/rtre-webbapplication/src/components/SearchBox.vue
@@ -2,7 +2,27 @@
 <div>
     <v-text-field solo label="Search" prepend-inner-icon="mdi-magnify" v-model="search"></v-text-field>
 
-    <ProjectList :projects="filteredProjects"></ProjectList>
+    <div v-if="search">
+        <v-expansion-panels v-for="project in filteredProjects" :key="project.id">
+            <v-expansion-panel>
+                <v-expansion-panel-header color="blue white--text" dark flat>
+                    {{project.name}}
+                </v-expansion-panel-header>
+                <v-expansion-panel-conent class="pt-5">
+                    <li>id: {{ project.oid }}</li>
+                    <li>Date: {{project.createdDate}}</li>
+                    <li>Schema: {{project.schema}}</li>
+                    <div v-if=" project.parentId != -1">
+                        <CheckOutIconButtonVue :oid="projects.oid" :schema="projects.schema" />
+                        <DeleteButtonVue :oid="projects.oid" />
+                    </div>
+                </v-expansion-panel-conent>
+            </v-expansion-panel>
+        </v-expansion-panels>
+    </div>
+    <div v-else>
+        <ProjectList :projects="filteredProjects"></ProjectList>
+    </div>
 
 </div>
 </template>
@@ -10,6 +30,8 @@
 <script>
 import axios from "axios";
 import ProjectList from "./ProjectList.vue";
+import CheckOutIconButtonVue from "./functionality/buttons/CheckOutIconButton.vue";
+import DeleteButtonVue from "./functionality/buttons/DeleteButton.vue";
 export default {
     name: "SearchBox",
     data() {
@@ -26,12 +48,15 @@ export default {
     },
     computed: {
         filteredProjects() {
-            console.log(this.projects.filter(project => project.oid.toString().includes(this.search.toString())|project.name.toLowerCase().includes(this.search.toLowerCase())) )
-            return this.projects.filter(project => project.oid.toString().includes(this.search.toString()) |project.name.toLowerCase().includes(this.search.toLowerCase()))
+            console.log(this.projects.filter(project => project.oid.toString().includes(this.search.toString()) | project.name.toLowerCase().includes(this.search.toLowerCase())))
+            return this.projects.filter(project => project.oid.toString().includes(this.search.toString()) | project.name.toLowerCase().includes(this.search.toLowerCase()))
         }
     },
     components: {
-        ProjectList
+        ProjectList,
+        CheckOutIconButtonVue,
+        DeleteButtonVue
+
     }
 }
 </script>
