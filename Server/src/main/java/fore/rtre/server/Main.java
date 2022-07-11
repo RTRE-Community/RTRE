0bba853954d72062e6011d6dfc6fac2f742983e9
Dennis Tran <61258188+TranDennis@users.noreply.github.com>
2022-07-11 09:20:17 +0200

    cypress test

--- Server/src/main/java/fore/rtre/server/Main.java
+++ Server/src/main/java/fore/rtre/server/Main.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 @CrossOrigin(origins="*")
 public class Main {
 	//public static String BimPort = "http://host.docker.internal:8082";
-	public static String BimPort = "http://localhost:8082";
+	public static String BimPort = "http://host.docker.internal:8082";
 
 	public static void main(String[] args) {
 		SpringApplication.run(Main.class, args);
